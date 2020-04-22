e8ef8001ec6e9cc8b08a9d295c0999bab99d5739
berishan <58296224+berishan@users.noreply.github.com>
2020-04-22 20:56:14 +0200

    gjenerimi i celesave RSA
    Signed-off-by: Nora Berisha <nora.berisha@studentet.uni-pr.edu>

--- CreateUser.java
+++ CreateUser.java
@@ -1,9 +1,15 @@
 import java.util.Base64;
-
+import javax.crypto.*;
+import java.security.*;
 public class CreateUser {
+    public static void main(String[] args) throws Exception{
 
 
-    public static void CreateUser() throws Exception {
+    }
+
+
+    public static void GenerateKeys() throws Exception {
+
         Base64.Encoder encoder = Base64.getEncoder();
         KeyPairGenerator keys = KeyPairGenerator.getInstance("RSA");
         keys.initialize(2048);
@@ -12,17 +18,17 @@ public class CreateUser {
         Key publicKey = keyPair.getPublic();
         Key privateKey = keyPair.getPrivate();
 
+        String privateKeyString = encoder.encodeToString(privateKey.getEncoded());
+        String publicKeyString = encoder.encodeToString(publicKey.getEncoded());
 
-        System.out.println("-----BEGIN RSA PRIVATE KEY-----\n");
-        System.out.println(encoder.encodeToString(privateKey.getEncoded()));
-        System.out.println("\n-----END RSA PRIVATE KEY-----\n");
-        System.out.println();
-        System.out.println("-----BEGIN RSA PUBLIC KEY-----\n");
-        System.out.println(encoder.encodeToString(publicKey.getEncoded()));
-        System.out.println("\n-----END RSA PUBLIC KEY-----\n");
+
+
+
+
+
 
 
     }
-}
+
 
 }
