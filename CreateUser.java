c5be92d390b64d0874a6c31aa7bb0fc56d2f50ca
berishan <58296224+berishan@users.noreply.github.com>
2020-04-22 21:03:36 +0200

    krijimi i xml files qe permbajne celesat RSA te gjeneruar
    Signed-off-by: Nora Berisha <nora.berisha@studentet.uni-pr.edu>

--- CreateUser.java
+++ CreateUser.java
@@ -1,6 +1,8 @@
+import java.io.File;
+import java.io.FileWriter;
 import java.util.Base64;
-import javax.crypto.*;
 import java.security.*;
+import javax.crypto.*;
 public class CreateUser {
     public static void main(String[] args) throws Exception{
 
@@ -8,7 +10,7 @@ public class CreateUser {
     }
 
 
-    public static void GenerateKeys() throws Exception {
+    public static void GenerateKeys(String name) throws Exception {
 
         Base64.Encoder encoder = Base64.getEncoder();
         KeyPairGenerator keys = KeyPairGenerator.getInstance("RSA");
@@ -21,12 +23,26 @@ public class CreateUser {
         String privateKeyString = encoder.encodeToString(privateKey.getEncoded());
         String publicKeyString = encoder.encodeToString(publicKey.getEncoded());
 
+        saveUser(name, privateKeyString, publicKeyString);
 
 
+    }
+    public static void saveUser(String name, String privateKey, String publicKey) throws Exception {
 
+        String publicfileName = "keys/" + name.replaceAll("[^A-Za-z0-9_]", "") + ".xml";
+        String privatefileName = "keys/" + name.replaceAll("[^A-Za-z0-9_]", "") + ".pub.xml";
 
+        FileWriter writeFile = new FileWriter(publicfileName);
+        writeFile.write("-----BEGIN RSA PUBLIC KEY-----\n");
+        writeFile.write(publicKey);
+        writeFile.write("\n-----END RSA PUBLIC KEY-----\n");
+        writeFile.close();
 
-
+        FileWriter writeAnotherFile = new FileWriter(privatefileName);
+        writeAnotherFile.write("-----BEGIN RSA PRIVATE KEY-----\n");
+        writeAnotherFile.write(privateKey);
+        writeAnotherFile.write("\n-----END RSA PRIVATE KEY-----\n");
+        writeAnotherFile.close();
 
     }
 
