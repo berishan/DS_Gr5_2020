622b90da0e03ec50b14b3b5e80e677fd69acac30
erisera19 <58450146+erisera19@users.noreply.github.com>
2020-04-25 14:12:11 +0200

    eksportimi i celesit ne console ose ne file te ri

---
+++ Export.java
@@ -0,0 +1,23 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.util.Scanner;
+
+public class Export {
+    static public String messageForError = "";
+    static public String fileName = "";
+    static public String messageWhenSaved = "";
+    static String data = "";
+
+
+
+    public static void exportKey(String name, String type, String path) throws Exception {
+
+        boolean hasPath;
+        if (path.equals("no path")) {
+            hasPath = false;
+        } else {
+            hasPath = true;
+        }
+    }
+    }
