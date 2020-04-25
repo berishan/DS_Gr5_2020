e36efd98f9397439072086559caab8e2cff7fc2a
berishan <58296224+berishan@users.noreply.github.com>
2020-04-25 22:32:17 +0200

    fshine celesat e perdoruesit qe deshirojme
    Signed-off-by: Nora Berisha <nora.berisha@studentet.uni-pr.edu>

--- DeleteUser.java
+++ DeleteUser.java
@@ -1,30 +1,39 @@
 import java.io.File;
+
 public class DeleteUser {
-    public static void deleteUser(String name) throws Exception{
-        String publicfileName = "keys/" + name.replaceAll("[^A-Za-z0-9_]", "") + ".xml";
-        String privatefileName = "keys/" + name.replaceAll("[^A-Za-z0-9_]", "") + ".pub.xml";
+    public static void deleteUser(String name) throws Exception {
 
-        File publicFile  = new File(publicfileName);
+        String privatefileName = "keys/" + name.replaceAll("[^A-Za-z0-9_]", "") + ".key";
+        String publicfileName = "keys/" + name.replaceAll("[^A-Za-z0-9_]", "") + ".pub.key";
+
+        File publicFile = new File(publicfileName);
         File privateFile = new File(privatefileName);
-        if(publicFile.exists() && privateFile.exists()){
+        if (publicFile.exists() && privateFile.exists()) {
+
             privateFile.delete();
             System.out.println("Eshte larguar celesi privat:    '" + privatefileName + "'.");
 
             publicFile.delete();
             System.out.println("Eshte larguar celesi publik:    '" + publicfileName + "'.");
-        } else if(publicFile.exists() && !privateFile.exists()){
+        }
+        else if (publicFile.exists() && !(privateFile.exists())) {
+
             publicFile.delete();
             System.out.println("Eshte larguar celesi publik:    '" + publicfileName + "'.");
-        } else if(!publicFile.exists() && privateFile.exists()){
+        }
+        else if (!(publicFile.exists()) && privateFile.exists()) {
+
             privateFile.delete();
             System.out.println("Eshte larguar celesi privat:    '" + privatefileName + "'.");
-        } else {
+
+        }
+        else {
+
             System.out.println("Gabim: Celesi '" + name + "' nuk ekziston.");
         }
 
 
     }
-
 
 
 }
