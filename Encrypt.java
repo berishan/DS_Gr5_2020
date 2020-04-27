feff0df6a480761def2234594c0eef7f756c37fd
diellas8 <58296399+diellas8@users.noreply.github.com>
2020-04-27 14:38:14 +0200

    Enkripton nje mesazh te dedikuar per nje shfrytezues.

--- Encrypt.java
+++ Encrypt.java
@@ -5,6 +5,10 @@ import java.util.Random;
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
+import java.io.FileReader;
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 
 
 
@@ -14,7 +18,7 @@ public class Encrypt {
     public static void main (String[] args) throws Exception{
         String name=base64Encode(args[0]);
         encodeBytes();
-        encryptDES();
+        encryptDES(args[0]);
         System.out.println(name);
 
 
@@ -39,32 +43,59 @@ public class Encrypt {
 
 
 
- public static void encryptDES() throws Exception {
-     Cipher ecipher;
+    public static void encryptDES(String name) throws Exception {
+        Cipher ecipher;
 
-     Cipher dcipher;
-     //byte[] key = new byte[8];
-     //new Random().nextBytes(key);
-     SecretKey key = KeyGenerator.getInstance("DES").generateKey();
-     byte[] data = key.getEncoded();
-     System.out.println(data.length);
-    // SecretKey key2 = new SecretKeySpec(data, 0, data.length, "DES");
+        Cipher dcipher;
+        //byte[] key = new byte[8];
+        //new Random().nextBytes(key);
+        SecretKey key = KeyGenerator.getInstance("DES").generateKey();
+        byte[] data = key.getEncoded();
+        encryptKey(name, data);
+        System.out.println(data.length);
+        // SecretKey key2 = new SecretKeySpec(data, 0, data.length, "DES");
 
-         ecipher = Cipher.getInstance("DES");
-         dcipher = Cipher.getInstance("DES");
-         ecipher.init(Cipher.ENCRYPT_MODE, key);
-         dcipher.init(Cipher.DECRYPT_MODE, key);
+        ecipher = Cipher.getInstance("DES");
+        dcipher = Cipher.getInstance("DES");
+        ecipher.init(Cipher.ENCRYPT_MODE, key);
+        dcipher.init(Cipher.DECRYPT_MODE, key);
 
 
-         // Encode the string into bytes using utf-8
+        // Encode the string into bytes using utf-8
         // byte[] utf8 = str.getBytes("UTF8");
 
-         // Encrypt
-         //byte[] enc = ecipher.doFinal(utf8);
-     }
- }
+        // Encrypt
+        //byte[] enc = ecipher.doFinal(utf8);
+    }
+    public static void encryptKey(String name, byte[] key) throws Exception {
+        String RSAkey = readPublicKey(name);
+        if(key.equals("gabim")){
 
-        // Encode byt
+        }
+        else{
+        }
+
+    }
+    public static String readPublicKey (String name) throws Exception {
+        String publicfileName = "keys/" + name.replaceAll("[^A-Za-z0-9_]", "") + ".pub.key";
+        BufferedReader Buff = new BufferedReader(new FileReader(publicfileName));
+        String firstLine = Buff.readLine();
+        if (firstLine.equals("-----BEGIN RSA PUBLIC KEY-----")){
+            String secondLine = Buff.readLine();
+            Buff.close();
+            return secondLine;
+
+        }
+        else{
+            System.out.println("File nuk permban celes publik RSA.");
+            Buff.close();
+            return "gabim";
+        }
+
+    }
+}
+
+// Encode byt
 
 
 //byte[] decoded = Base64.getDecoder().decode(encoded);
